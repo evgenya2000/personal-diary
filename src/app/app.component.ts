641729ef32e2cc4b2e8ff0e2c50d9d91fe96c78d
evgenya2000 <evgenya.2000@yandex.ru>
2023-08-23 23:12:44 +0300

    Добавлена функция сортировки по дате

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,20 @@ export class AppComponent {
   // метод обновления note array
   refreshNotes(){
     this.service.getNotes().subscribe((res)=>{
+      // Сортировка массива записей по полю `date`
+      res.sort((a, b) => {
+        const dateA = a["date"];
+        const dateB = b["date"];
+        
+        if (dateA > dateB) {
+          return -1;
+        } else if (dateA < dateB) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+
       this.notes = res;
     })
   }
@@ -23,12 +37,14 @@ export class AppComponent {
     this.refreshNotes();
   }
 
+  // Метод добавления новых записей
   addNotes(newNotes:string){
     this.service.addNote(newNotes).then(()=>{
       this.refreshNotes();
     })
   }
 
+  // Метод удаления записей
   deleteNotes(id:string){
     this.service.deleteNote(id).then(()=>{
       this.refreshNotes();
